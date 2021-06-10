354df341edc1dddfd100a975a256ac122f1b3ed5
Vice <vice.roncevic@luxc.tech>
2021-06-10 11:01:09 +0200

    Range image pcl tutorial working with rectangular point cloud

---
+++ catkin_ws/build/data_pre_processing/CMakeFiles/visualize_range_image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/luxc/vice_ROS_OD_SC/catkin_ws/devel/lib/data_pre_processing/visualize_range_image"
+  "/home/luxc/vice_ROS_OD_SC/catkin_ws/devel/lib/data_pre_processing/visualize_range_image.pdb"
+  "CMakeFiles/visualize_range_image.dir/src/visualize_range_image.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visualize_range_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
